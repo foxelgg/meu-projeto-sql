f28572624454481883d4716caeecc2ff95aa40d3
foxelgg <dsgustavoalmeida@gmail.com>
2025-12-08 21:46:18 -0300

    Melhorias nos comentários das queries

--- src/raw/01_criando_tabelas_raw.sql
+++ src/raw/01_criando_tabelas_raw.sql
@@ -4,12 +4,14 @@
     - Todos definidos como TEXT para evitar erros na ingestão.
     ======================================================================================== */
 
+-- Tabela de clientes 
 CREATE TABLE customers_raw (
     customer_id TEXT,
     customer_name TEXT,
     customer_city TEXT
 );
 
+-- Tabela de itens do pedido
 CREATE TABLE order_items_raw (
     order_id TEXT,
     product_id TEXT,
@@ -17,6 +19,7 @@ CREATE TABLE order_items_raw (
     freight_value TEXT
 );
 
+-- Tabela de pedidos
 CREATE TABLE orders_raw (
     order_id TEXT,
     customer_id TEXT,
@@ -24,12 +27,14 @@ CREATE TABLE orders_raw (
     delivered_at TEXT
 );
 
+-- Tabela de pagamentos
 CREATE TABLE payments_raw (
     order_id TEXT,
     payment_type TEXT,
     payment_value TEXT
 );
 
+-- Tabela de produtos
 CREATE TABLE products_raw (
     product_id TEXT,
     product_name TEXT,
